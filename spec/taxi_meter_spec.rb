a955926d54896dafd7a17cbe025f2d701b3271ab
Micah Stewart <stewart.micah@gmail.com>
2014-02-14 11:58:37 -0600

    Add function for start and stop time, and mileage calculator

--- spec/taxi_meter_spec.rb
+++ spec/taxi_meter_spec.rb
@@ -1,4 +1,5 @@
 require './taxi_meter.rb'
+require 'pry-debugger'
 
 describe TaxiMeter do
 
@@ -41,7 +42,21 @@ describe TaxiMeter do
       expect(@meter.start_time).to eq(start_time)
     end
 
-    it "records the time it stopped"
+    it "records the time it stopped" do
+      #freeze time to the point when the meter stops
+      stop_time = Time.now
+      Time.stub(:now).and_return(stop_time)
+
+      # this should grab the current time
+      @meter.stop
+
+      #re-stub Time to be 5min in the future
+      Time.stub(:now).and_return(stop_time + 5 * 60)
+
+      # once stopped, stop_time shouldn't rely on current time
+      expect(@meter.stop_time).to eq(stop_time)
+    end
+
   end
 
   context "The taxi meter starts" do
@@ -54,9 +69,59 @@ describe TaxiMeter do
       @meter.start
     end
 
-    it "charges $2.50 for the first 1/6 mile (recorded in cents)"
+    it "charges $2.50 for the first 1/6 mile (recorded in cents)" do
+      @meter.miles_driven = 1.0 / 6.0
+      expect(@meter.amount_due).to eq(250)
+    end
+
+    it "charges correctly for 2 miles driven" do
+      @meter.miles_driven = 2
+
+      expect(@meter.amount_due).to eq(690)
+    end
   end
 
+  # context "The taxi meter records waiting time" do
+  #   before do
+  #     @meter = TaxiMeter.new
+  #   end
+
+  #   it "checks the wait time" do
+  #     # Copy/Paste previous start time test
+  #     start_time = Time.now
+  #     Time.stub(:now).and_return(start_time)
+
+  #     # This should grab the current time
+  #     @meter.start
+
+  #     # Re-stub Time to be 5 minutes into the future
+  #     Time.stub(:now).and_return(start_time + 5 * 60)
+
+  #     # Once started, start_time shouldn't rely on the current time
+  #     expect(@meter.start_time).to eq(start_time)
+
+  #     # Copy/Paste the previous stop time test
+  #     #freeze time to the point when the meter stops
+  #     stop_time = Time.now
+  #     Time.stub(:now).and_return(stop_time)
+
+  #     # this should grab the current time
+  #     @meter.stop
+
+  #     #re-stub Time to be 5min in the future
+  #     Time.stub(:now).and_return(stop_time + 30 * 60)
+
+  #     # once stopped, stop_time shouldn't rely on current time
+  #     expect(@meter.stop_time).to eq(stop_time)
+
+
+  #     expect(@meter.waiting_time).to eq(30)
+  #   end
+
+  #   xit "checks the wait cost" do
+  #     expect(@meter.waiting_time).to eq(0)
+  #   end
+  # end
 
   context "The taxi meter starts from ABIA" do
     before do
@@ -68,7 +133,7 @@ describe TaxiMeter do
       @meter.start
     end
 
-    it "has a minimum fare of $13.10"
+    xit "has a minimum fare of $13.10"
   end
 
 end

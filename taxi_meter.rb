fc67ad06e5df92e9e3af30008a068d472320c64f
Micah Stewart <stewart.micah@gmail.com>
2014-02-16 22:57:57 -0600

    complete logic to bill minimum amount if travelling ot ABIA

--- taxi_meter.rb
+++ taxi_meter.rb
@@ -2,9 +2,10 @@ class TaxiMeter
   attr_accessor :amount_due, :miles_driven, :airport
   attr_reader :start_time, :stop_time
 
-  def initialize
+  def initialize(airport: false)
     @miles_driven = 0
     @amount_due = 0
+    @airport = airport
   end
 
   def start
@@ -20,6 +21,12 @@ class TaxiMeter
     mileage_cost = 40 * (@miles_driven * 6.0 - 1)
     wait_cost = (Time.now - @start_time) / 60.0 * 2900.0 / 60.0
     current_due = (initial_cost + mileage_cost + wait_cost).round
+
+    if @airport && current_due < 1310
+      return 1310
+    else
+      return current_due
+    end
   end
 
 end
